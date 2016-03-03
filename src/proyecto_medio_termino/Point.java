e1ee36e3c6875632655ccd58c0d26b2e30d1980b
Luis Pujols <luispujols@hotmail.com>
2016-03-03 19:18:50 -0400

    ADD "Created the main classes for our tennis project. (Game,  Player,  Point, Set and Shots"

---
+++ src/proyecto_medio_termino/Point.java
@@ -0,0 +1,7 @@
+package proyecto_medio_termino;
+
+/**
+ * Created by Luis on 3/3/2016.
+ */
+public class Point {
+}
---
+++ src/proyecto_medio_termino/Set.java
@@ -0,0 +1,11 @@
+package proyecto_medio_termino;
+
+/**
+ * Created by Luis on 3/3/2016.
+ */
+public class Set {
+
+
+
+
+}
