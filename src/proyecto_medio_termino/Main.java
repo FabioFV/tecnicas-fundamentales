4a7d8c6c7f398370bd45fe98f441589bca197a94
Luis Pujols <luispujols@hotmail.com>
2016-03-08 18:33:26 -0400

    UPDATE "Updated the Main Class to add the Menu and its respective methods."

--- src/proyecto_medio_termino/Main.java
+++ src/proyecto_medio_termino/Main.java
@@ -3,10 +3,53 @@
  */
 package proyecto_medio_termino;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
+        System.out.println("-----Menu----");
+        System.out.println("1) Register Match");
+        System.out.println("2) Resimulate old match");
+        System.out.println("3) Exit.");
+
+        Scanner in = new Scanner(System.in);
+
+        int option;
+
+        option = in.nextInt();
+
+        switch (option) {
+
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                exit();
+                break;
+            default:
+                System.out.println("Please enter a valid option");
+                break;
+        }
+
+
+
+    }
+
+    private static void newMatch (){
+        ////TODO: LOGIC OF newMatch
+
+    }
+
+    private static void findMatch(){
+        ////TODO: LOGIC of findMatch
+
+    }
+
+    private static void exit(){
+
+        System.out.println("Goodbye.");
 
     }
 
