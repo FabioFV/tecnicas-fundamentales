519acf36a1b6c2b9164afaf28775fa2ad3008019
Luis Pujols <luispujols@hotmail.com>
2016-03-08 23:56:28 -0400

    UPDATE "Added the newMatch method functionality for the Main Class. [This includes the creation of both players and all their detail's as well as creating a new Match with all the info recolected from the user]."

--- src/proyecto_medio_termino/Main.java
+++ src/proyecto_medio_termino/Main.java
@@ -18,10 +18,12 @@ public class Main {
         int option;
 
         option = in.nextInt();
-
+        in.close();
+        
         switch (option) {
 
             case 1:
+                newMatch();
                 break;
             case 2:
                 break;
@@ -38,8 +40,60 @@ public class Main {
     }
 
     private static void newMatch (){
-        ////TODO: LOGIC OF newMatch
 
+        String player1Name, player1LastName, player1Handed;
+        String player2Name, player2LastName, player2Handed;
+        String tieBreakGame;
+        boolean tieBreakConfirm;
+
+        int serverPlayer, numSets;
+
+        Scanner inRm = new Scanner(System.in);
+
+        System.out.println("Insert the name for player 1:");
+        player1Name = inRm.nextLine();
+        System.out.println("Insert the last name for player 1:");
+        player1LastName = inRm.nextLine();
+        System.out.println("Is " + player1Name + " " + player1LastName + " Right Handed or Left Handed?");
+        player1Handed = inRm.nextLine();
+
+        System.out.println("Insert the name for player 2:");
+        player2Name = inRm.nextLine();
+        System.out.println("Insert the last name for player 2:");
+        player2LastName = inRm.nextLine();
+        System.out.println("Is " + player2Name + " " + player2LastName + " Right Handed or Left Handed?");
+        player2Handed = inRm.nextLine();
+
+        System.out.println("Choose the number of Sets for this game:");
+        System.out.println("3 or 5?");
+        numSets = inRm.nextInt();
+
+        System.out.println("Will this match allow tie breaks?");
+        System.out.println("Yes or No?");
+        tieBreakGame = inRm.nextLine();
+
+        if( tieBreakGame == "yes" || tieBreakGame == "Yes") tieBreakConfirm = true;
+        else tieBreakConfirm = false;
+
+        System.out.println("Choose which player will Serve first?");
+        System.out.println("1)" + player1Name + " " + player1LastName);
+        System.out.println("2)" + player2Name + " " + player2LastName);
+        serverPlayer = inRm.nextInt();
+
+        inRm.close();
+
+        Player Player1 = new Player(player1Name, player1LastName, player1Handed);
+        Player Player2 = new Player(player2Name, player2LastName, player2Handed);
+        Player choosedServerPlayer;
+
+        if(serverPlayer == 1)  choosedServerPlayer = Player1;
+        else choosedServerPlayer = Player2;
+
+        ////TODO Finish New Match Logic, REMEMBER: TO FIX the Line 94 in the Print of the Winner so it says the name of the winner.
+        Match newMatch = new Match(Player1, Player2, choosedServerPlayer, numSets, tieBreakConfirm);
+        Player matchWinner = newMatch.getWinner();
+
+        System.out.println("The Match winner is" + matchWinner);
     }
 
     private static void findMatch(){
