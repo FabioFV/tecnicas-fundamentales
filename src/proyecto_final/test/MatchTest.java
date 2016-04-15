22a4a6fed1fb4088e769ffa5471e364716195f3b
Fabio Emmanuel Ferreras Vasquez <fabio_ferreras@hotmail.com>
2016-04-15 00:04:33 -0400

    Adding two more test

--- src/proyecto_final/test/MatchTest.java
+++ src/proyecto_final/test/MatchTest.java
@@ -16,7 +16,7 @@ import static org.junit.Assert.*;
 public class MatchTest {
 
     @Test
-    public void addPoint() throws Exception {
+    public void addPointSingle() throws Exception {
         Match match = Match.getInstance();
         Player p1 = new Player("Player", "1", "Right");
         Player p2 = new Player("Player", "2", "Left");
@@ -29,7 +29,7 @@ public class MatchTest {
     }
 
     @Test
-    public void addPointWithTiebreak() throws Exception {
+    public void addPointWithTiebreakSingle() throws Exception {
         Match match = Match.getInstance();
         Player p1 = new Player("Player", "1", "Right");
         Player p2 = new Player("Player", "2", "Left");
@@ -38,6 +38,78 @@ public class MatchTest {
         for (int i = 0; i < 47; i++) {
             match.addPoint(new Point(p1, Shots.BACKHAND));
         }
+        assertEquals(true, match.addPoint(new Point(p1, Shots.BACKHAND)));
+    }
+
+    @Test
+    public void addPoint() throws Exception {
+        Match match = Match.getInstance();
+        Player p1 = new Player("Player", "1", "Right");
+        Player p2 = new Player("Player", "2", "Left");
+        match.init(p1,p2,p1,3,false);
+
+        for (int i = 0; i < 24; i++) {
+            match.addPoint(new Point(p1, Shots.BACKHAND));
+        }
+        // 1 - 0
+
+        for (int i = 0; i < 24; i++) {
+            match.addPoint(new Point(p2, Shots.BACKHAND));
+        }
+        // 1 - 1
+
+        for (int i = 0; i < 24; i++) {
+            match.addPoint(new Point(p2, Shots.BACKHAND));
+        }
+        // 1 - 2
+
+        for (int i = 0; i < 24; i++) {
+            match.addPoint(new Point(p1, Shots.BACKHAND));
+        }
+        // 2 - 2
+
+        for (int i = 0; i < 23; i++) {
+            match.addPoint(new Point(p1, Shots.BACKHAND));
+        }
+        assertEquals(true, match.addPoint(new Point(p1, Shots.BACKHAND)));
+    }
+
+    @Test
+    public void addPointWithTiebreak() throws Exception {
+        Match match = Match.getInstance();
+        Player p1 = new Player("Player", "1", "Right");
+        Player p2 = new Player("Player", "2", "Left");
+        match.init(p1,p2,p1,2,true);
+
+        for (int i = 0; i < 24; i++) {
+            match.addPoint(new Point(p1, Shots.BACKHAND));
+        }
+        // 1 - 0
+
+        for (int i = 0; i < 24; i++) {
+            match.addPoint(new Point(p2, Shots.BACKHAND));
+        }
+        // 1 - 1
+
+        for (int i = 0; i < 20; i++) {
+            match.addPoint(new Point(p1, Shots.BACKHAND));
+        }
+        for (int i = 0; i < 20; i++) {
+            match.addPoint(new Point(p2, Shots.BACKHAND));
+        }
+
+        for (int i = 0; i < 4; i++) {
+            match.addPoint(new Point(p1, Shots.BACKHAND));
+        }
+        for (int i = 0; i < 4; i++) {
+            match.addPoint(new Point(p2, Shots.BACKHAND));
+        }
+
+
+        for (int i = 0; i < 6; i++) {
+            match.addPoint(new Point(p1, Shots.BACKHAND));
+        }
+
         assertEquals(true, match.addPoint(new Point(p1, Shots.BACKHAND)));
     }
 
